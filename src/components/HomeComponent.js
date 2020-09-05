50836a9112ea87ded0d3fa1f70a2eac4fc89ca52
Prarthana <prarthanashr04@gmail.com>
2020-09-05 21:49:09 +0530

    fetch handling errors

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderCard({ item, isLoading, errMess }) {
+    console.log(item+ isLoading +errMess)
     if (isLoading) {
         return (
             <Loading />
@@ -14,18 +16,19 @@ function RenderCard({ item, isLoading, errMess }) {
         );
     }
     else
-        return (
+        return (item?
             <Card>
-                <CardImg src={item.image} alt={item.name} />
+                <CardImg src={baseUrl + item.image} alt={item.name} />
                 <CardBody>
                     <CardTitle>{item.name}</CardTitle>
                     {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
                     <CardText>{item.description}</CardText>
                 </CardBody>
-            </Card>
+            </Card>:null
         );
 }
 function Home(props) {
+    console.log(props);
     return (
         <div className="container">
             <div className="row align-items-start">
@@ -35,7 +38,9 @@ function Home(props) {
                         errMess={props.dishesErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.promotion} />
+                    <RenderCard item={props.promotion} 
+                        isLoading={props.promosLoading}
+                        errMess={props.promosErrMess}/>
                 </div>
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.leader} />
