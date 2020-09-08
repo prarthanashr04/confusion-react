7368e26a76d717b4299493aeb035ca9c2f97612f
Prarthana <prarthanashr04@gmail.com>
2020-09-08 22:48:34 +0530

    Build Ready

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -38,10 +38,6 @@ const mapDispatchToProps = dispatch => ({
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
-
-  }
 
   componentDidMount() {
     this.props.fetchDishes();
